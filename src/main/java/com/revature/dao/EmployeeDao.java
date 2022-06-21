ad0be958abc9e8647956a55fed4b8fcbb8d22842
Jheflin1995 <106621400+Jheflin1995@users.noreply.github.com>
2022-06-21 14:49:09 -0500

    update

--- src/main/java/com/revature/dao/EmployeeDao.java
+++ src/main/java/com/revature/dao/EmployeeDao.java
@@ -5,41 +5,53 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-import com.revature.models.Employees;
+import com.revature.models.Employee;
 import com.revature.util.HibernateUtil;
 
+// servlet -> calls service --> calls dao
 public class EmployeeDao {
-//CRUD
 	
-	public int insert(Employees e) {
+	// CRUD methods
+	
+	// Create (think that in the service layer we'll have a REGISTER()
+	public int insert(Employee e) {
 		
+		// grab the session object
 		Session ses = HibernateUtil.getSession();
 		
+		// begin a tx
 		Transaction tx = ses.beginTransaction();
 		
+		// capture the pk returned when the session method save() is called
 		int pk = (int) ses.save(e);
 		
+		// return the pk
 		return pk;
 		
 	}
 	
-	public List<Employees>findAll(){
+	// Read
+	public List<Employee> findAll() {
 		
+		// grab the session
 		Session ses = HibernateUtil.getSession();
 		
-		//HQL - Hibernate Query Langauge: odd mix of OOP and Native SQL
+		// make an HQL -- Hibernate Query Language: odd mix of OOP & native SQL
+		 List<Employee> emps = ses.createQuery("from Employee", Employee.class).list();
 		
-		List<Employees>emps = ses.createQuery("from Employees", Employees.class).list();
-		
+		 // return the list of employees
 		return emps;
 		
 	}
 	
 	public boolean delete(int id) {
 		return false;
+		
 	}
 	
-	public boolean update(Employees e) {
+	public boolean update(Employee e) {
 		return false;
 	}
+	
+
 }
--- src/main/java/com/revature/service/EmployeeService.java
+++ src/main/java/com/revature/service/EmployeeService.java
@@ -4,31 +4,50 @@ import java.util.List;
 import java.util.Optional;
 
 import com.revature.dao.EmployeeDao;
-import com.revature.models.Employees;
+import com.revature.models.Employee;
 
 public class EmployeeService {
-
+	
 	private EmployeeDao edao;
 	
+	/**
+	 * Dependency Injection via Constructor Injection
+	 * 
+	 * Constructor Injection is a sophisticated way of ensuring 
+	 * that the EmployeeService object ALWAYS has an EmployeeDao object
+	 * 
+	 */
 	public EmployeeService(EmployeeDao edao) {
 		
 		this.edao = edao;
+		
 	}
 	
-	public Employees confirmLgoin(String username, String password) {
+	/**
+	 * Our Servlet will pass the username and the password to this method invocation
+	 * @param username
+	 * @param password
+	 * @return
+	 */
+	public Employee confirmLogin(String username, String password) {
 		
-		Optional<Employees> possibleEmp = edao.findAll().stream()
-				.filter(e ->(e.getUsername().equals(username) && e.getPassword().equals(password)))
+		// let's stream through all the employees that are returned
+		Optional<Employee> possibleEmp = edao.findAll().stream()
+				.filter(e -> (e.getUsername().equals(username) && e.getPassword().equals(password)))
 				.findFirst();
 		
-		return (possibleEmp.isPresent() ? possibleEmp.get() : new Employees());
+		// IF the employee is present, return it, otherwise return empty Emp object (with id of 0)
+		return (possibleEmp.isPresent() ? possibleEmp.get() : new Employee());
+		// ideally you should optimize this and set up a custom exception to be returned
+	}
+	
+	public List<Employee> getAll() {
+		
+		return edao.findAll();
 		
 	}
 	
-	public List<Employees> getAll(){
-		
-		return edao.findAll();
-		}
+	
+	
+
 }
-
-
